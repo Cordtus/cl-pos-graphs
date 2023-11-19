e56df51c587f46d2ac49e5b32ac0b44cbfde0274
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-11-18 21:29:01 -0700

    Update graph-log.py

--- graph-log.py
+++ graph-log.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 
 def read_data(file_path):
     # Read the JSON data file
@@ -15,7 +16,7 @@ def preprocess_data(data):
     df['lower_tick'] = pd.to_numeric(df['lower_tick'])
     return df
 
-def plot_liquidity_vs_ticks(df):
+def plot_liquidity_vs_ticks(df, output_path):
     # Sort the DataFrame by 'lower_tick' to create a meaningful line chart
     sorted_df = df.sort_values('lower_tick')
 
@@ -27,10 +28,15 @@ def plot_liquidity_vs_ticks(df):
     plt.yscale('log')  # Set y-axis to logarithmic scale
     plt.title('Logarithmic Line Chart of Liquidity Amounts vs. Lower Tick')
     plt.grid(True)
-    plt.show()
+    plt.savefig(output_path)  # Save the plot as a PNG file
+    plt.close()  # Close the plot
 
 if __name__ == "__main__":
-    file_path = 'path_to_your_file.json'  # Replace with the actual path to your JSON file
-    data = read_data(file_path)
+    parser = argparse.ArgumentParser(description='Generate a logarithmic line chart from JSON data.')
+    parser.add_argument('file_path', type=str, help='Path to the JSON file containing the dataset')
+    parser.add_argument('output_path', type=str, help='Path to save the output PNG file')
+    args = parser.parse_args()
+
+    data = read_data(args.file_path)
     df = preprocess_data(data)
-    plot_liquidity_vs_ticks(df)
+    plot_liquidity_vs_ticks(df, args.output_path)
