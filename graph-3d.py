4de88027abfdc2f7dc40b054a4ada5e3f1326e5f
Cordtus <96965330+Cordtus@users.noreply.github.com>
2023-11-19 00:56:13 -0700

    Update graph-3d.py

--- graph-3d.py
+++ graph-3d.py
@@ -16,14 +16,21 @@ def preprocess_data(data):
     df['liquidity_amount'] = pd.to_numeric(df['liquidity_amount'])
     df['lower_tick'] = pd.to_numeric(df['lower_tick'])
     df['upper_tick'] = pd.to_numeric(df['upper_tick'])
+    # Calculate tick range
+    df['tick_range'] = df['upper_tick'] - df['lower_tick']
     return df
 
 def plot_3d_liquidity(df, output_path):
-    fig = plt.figure(figsize=(15, 10))
+    # Plotting
+    fig = plt.figure(figsize=(12, 8))
     ax = fig.add_subplot(111, projection='3d')
 
-    # 3D Scatter plot
-    scatter = ax.scatter(df['lower_tick'], df['upper_tick'], df['liquidity_amount'], c=df['liquidity_amount'], cmap='viridis', marker='o')
+    # Focusing on a smaller range where most data lies
+    focus_df = df[(df['liquidity_amount'] < df['liquidity_amount'].quantile(0.99)) &
+                  (df['tick_range'] < df['tick_range'].quantile(0.99))]
+
+    img = ax.scatter(focus_df['lower_tick'], focus_df['upper_tick'], focus_df['liquidity_amount'],
+                     c=focus_df['liquidity_amount'], cmap='viridis', marker='o', depthshade=True)
 
     # Labels and title
     ax.set_xlabel('Lower Tick Value')
@@ -31,15 +38,15 @@ def plot_3d_liquidity(df, output_path):
     ax.set_zlabel('Liquidity Amount')
     ax.set_title('3D Plot of Liquidity Amount vs. Tick Range')
 
-    # Colorbar for liquidity amounts
-    fig.colorbar(scatter, ax=ax, label='Liquidity Amount')
+    # Color bar
+    fig.colorbar(img, ax=ax, label='Liquidity Amount')
 
-    # Save the plot
+    # Save plot to file
     plt.savefig(output_path)
     plt.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Generate a 3D plot from JSON data.')
+    parser = argparse.ArgumentParser(description='Generate a 3D scatter plot from JSON data.')
     parser.add_argument('file_path', type=str, help='Path to the JSON file containing the dataset')
     parser.add_argument('output_path', type=str, help='Path to save the output PNG file')
     args = parser.parse_args()
